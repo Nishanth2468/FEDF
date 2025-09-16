ffc96da533d9c0df43d5960dffbab5105f911f65
Nishanth2468 <your_email@example.com>
2025-09-16 08:55:22 +0530

    Some changes

--- Weather_app/Script_1.js
+++ Weather_app/Script_1.js
@@ -1,9 +1,11 @@
 let id = '27c370568c0d37820bdb9e21181522b9';
 let url = 'https://api.openweathermap.org/data/2.5/weather?units=metric&appid=' + id;
 
-let city = document.querySelector('.name');
+let city = document.querySelector('.name figcaption');
+let flag = document.querySelector('.name img');
 let form = document.querySelector("form");
-let temperature = document.querySelector('.temperature');
+let temperature = document.querySelector('.temperature span');
+let tempIcon = document.querySelector('.temperature img');
 let description = document.querySelector('.description');
 let valueSearch = document.getElementById('name');
 let clouds = document.getElementById('clouds');
@@ -13,24 +15,24 @@ let main = document.querySelector('main');
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    if (valueSearch.value !== '') {
-        searchWeather();
+    if (valueSearch.value.trim() !== '') {
+        searchWeather(valueSearch.value.trim());
     }
 });
 
-const searchWeather = () => {
-    fetch(url + '&q=' + valueSearch.value)
+const searchWeather = (cityName) => {
+    fetch(url + '&q=' + cityName)
         .then(response => response.json())
         .then(data => {
             console.log(data);
             if (data.cod == 200) {
                 // City + flag
-                city.querySelector('figcaption').innerText = data.name;
-                city.querySelector('img').src = `https://flagsapi.com/${data.sys.country}/shiny/32.png`;
+                city.innerText = data.name;
+                flag.src = `https://flagsapi.com/${data.sys.country}/shiny/32.png`;
 
                 // Temperature + weather icon
-                temperature.querySelector('img').src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`;
-                temperature.querySelector('span').innerText = Math.round(data.main.temp);
+                tempIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`;
+                temperature.innerText = Math.round(data.main.temp);
 
                 // Weather description
                 description.innerText = data.weather[0].description;
@@ -52,7 +54,6 @@ const searchWeather = () => {
 
 // Default search on load
 const initApp = () => {
-    valueSearch.value = 'Washington';
-    searchWeather();
+    searchWeather('Washington');
 };
 initApp();
